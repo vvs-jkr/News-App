ad94493f2c526b89f15c67b094ce2a3f4b938328
VS <vvs.jkr@gmail.com>
2024-03-11 12:34:13 +0300

    refactoring code

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.css'
 import NewsItem from '../../components/NewsItem/NewsItem'
+import withSkeleton from '../../helpers/hocs/withSkeleton'
 
 const NewsList = ({ news }) => {
   return (
@@ -11,4 +12,6 @@ const NewsList = ({ news }) => {
   )
 }
 
-export default NewsList
+const NewsListWithSkeleton = withSkeleton(NewsList, 'item', 10)
+
+export default NewsListWithSkeleton
