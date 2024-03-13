9b6a517848a4f96edb3fee12a9dc1042f31ee2b7
VS <vvs.jkr@gmail.com>
2024-03-13 17:45:10 +0300

    migrate from js to ts

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,17 +1,22 @@
 import styles from './styles.module.css'
-import NewsItem from '../../components/NewsItem/NewsItem'
+import NewsItem from '../NewsItem/NewsItem'
 import withSkeleton from '../../helpers/hocs/withSkeleton'
+import { INews } from '../../interfaces'
 
-const NewsList = ({ news }) => {
+interface Props {
+  news?: INews[]
+}
+
+const NewsList = ({ news }: Props) => {
   return (
     <ul className={styles.list}>
-      {news.map((item) => {
+      {news?.map((item) => {
         return <NewsItem key={item.id} item={item} />
       })}
     </ul>
   )
 }
 
-const NewsListWithSkeleton = withSkeleton(NewsList, 'item', 10)
+const NewsListWithSkeleton = withSkeleton<Props>(NewsList, 'item', 10)
 
 export default NewsListWithSkeleton
